e4f269b0a60da8d9505ab20cf97294b5d2549ce8
LeonAdeoye <leon.adeoye@gmail.com>
2021-06-20 22:20:50 +0800

    added logger

--- src/main/java/com/leon/disruptor/ClientCashPositionService.java
+++ src/main/java/com/leon/disruptor/ClientCashPositionService.java
@@ -3,12 +3,24 @@ package com.leon.disruptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ClientCashPositionService implements PositionService
 {
     private static final Logger logger = LoggerFactory.getLogger(ClientCashPositionService.class);
 
+    private Map<Integer, Double> mapOfCash = new HashMap<>();
+
     public PositionResponse check(PositionRequest positionRequest)
     {
-        return new PositionResponse(100, OutcomeType.SUCCESS, positionRequest.getClientId(), positionRequest.getRIC(), positionRequest.getEventType(), positionRequest.getRequestType());
+        if(mapOfCash.containsKey(positionRequest.getClientId()))
+        {
+            // TODO refine logic
+            if(positionRequest.getDelta() <= mapOfCash.get(positionRequest.getClientId()))
+                return new PositionResponse(mapOfCash.get(positionRequest.getClientId()) - positionRequest.getDelta(), OutcomeType.SUCCESS, positionRequest.getClientId(), positionRequest.getRIC(), positionRequest.getEventType(), positionRequest.getRequestType());
+        }
+        else
+            return new PositionResponse(0, OutcomeType.SUCCESS, positionRequest.getClientId(), positionRequest.getRIC(), positionRequest.getEventType(), positionRequest.getRequestType());
     }
 }
