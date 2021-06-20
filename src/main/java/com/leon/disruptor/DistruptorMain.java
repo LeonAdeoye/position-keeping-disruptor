c7c04d8b0c52638a7df41a04a9b1abe3687c47eb
LeonAdeoye <leon.adeoye@gmail.com>
2021-06-20 13:16:55 +0800

    first commit

---
+++ src/main/java/com/leon/disruptor/DistruptorMain.java
@@ -0,0 +1,9 @@
+package com.leon.disruptor;
+
+public class DistruptorMain
+{
+    public static void main(String[] args)
+    {
+        System.out.println("\nStarting program now...\n");
+    }
+}
