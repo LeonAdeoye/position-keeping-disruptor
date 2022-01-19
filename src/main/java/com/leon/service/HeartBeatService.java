e37c168950e9894e8a24097aa24377dddf599571
LeonAdeoye <leon.adeoye@gmail.com>
2022-01-19 23:37:37 +0800

    Added heartbeat service shell

---
+++ src/main/java/com/leon/service/HeartBeatService.java
@@ -0,0 +1,9 @@
+package com.leon.service;
+
+public interface HeartBeatService
+{
+	void ping();
+	void respond();
+	void start();
+	void stop();
+}
---
+++ src/main/java/com/leon/service/HeartBeatServiceImpl.java
@@ -0,0 +1,28 @@
+package com.leon.service;
+
+public class HeartBeatServiceImpl implements HeartBeatService
+{
+	@Override
+	public void ping()
+	{
+
+	}
+
+	@Override
+	public void respond()
+	{
+
+	}
+
+	@Override
+	public void start()
+	{
+
+	}
+
+	@Override
+	public void stop()
+	{
+
+	}
+}
