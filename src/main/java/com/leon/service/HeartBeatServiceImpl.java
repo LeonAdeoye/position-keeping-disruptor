200d7357400d0dd519d85827e3b6b797acb291c8
LeonAdeoye <leon.adeoye@gmail.com>
2022-02-05 23:29:59 +0800

    heartbeat service

--- src/main/java/com/leon/service/HeartBeatServiceImpl.java
+++ src/main/java/com/leon/service/HeartBeatServiceImpl.java
@@ -8,12 +8,12 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 
 @Service
 public class HeartBeatServiceImpl implements HeartBeatService, MessageListener
@@ -27,8 +27,10 @@ public class HeartBeatServiceImpl implements HeartBeatService, MessageListener
 	@Value("${heartbeat.check.maximum.interval}")
 	long heartbeatCheckMaximumInterval;
 	private boolean isPrimary;
-	private String sender;
+	private String heartbeatSender;
+	private String heartbeatUID;
 	private LocalDateTime lastTimeStamp;
+	private String uid = UUID.randomUUID().toString();
 
 	@Scheduled(fixedDelay=100)
 	public void ping()
@@ -36,7 +38,7 @@ public class HeartBeatServiceImpl implements HeartBeatService, MessageListener
 		if(!hasStarted)
 			return;
 
-		jmsTemplate.send(heartbeatTopic, s -> s.createTextMessage(isPrimary? "PRIMARY" : "SECONDARY"));
+		jmsTemplate.send(heartbeatTopic, s -> s.createTextMessage((isPrimary? "PRIMARY" : "SECONDARY") + "=" + uid));
 	}
 
 	@Override
@@ -51,12 +53,24 @@ public class HeartBeatServiceImpl implements HeartBeatService, MessageListener
 			if(message instanceof TextMessage)
 			{
 				TextMessage textMessage = (TextMessage) message;
-				sender = textMessage.getText();
-				if(sender.equals(isPrimary ? "PRIMARY" : "SECONDARY"))
+				String[] splitHeartbeat  = textMessage.getText().split("=");
+				if(splitHeartbeat.length == 2)
+				{
+					heartbeatSender = splitHeartbeat[0];
+					heartbeatUID = splitHeartbeat[1];
+				}
+				else
 					return;
 
+				if(heartbeatSender.equals(isPrimary ? "PRIMARY" : "SECONDARY"))
+				{
+					if(!splitHeartbeat[1].equals(uid))
+						logger.error("Another service is also running as {} with a UID: {}", (isPrimary ? "PRIMARY" : "SECONDARY"), heartbeatUID);
+					return;
+				}
+
 				lastTimeStamp = LocalDateTime.now();
-				logger.info("Received heartbeat message from: {} at: {}", sender, lastTimeStamp);
+				logger.info("Received heartbeat message from: {} with UID: {} at: {}", heartbeatSender, heartbeatUID, lastTimeStamp);
 			}
 		}
 		catch(Exception e)
@@ -65,15 +79,14 @@ public class HeartBeatServiceImpl implements HeartBeatService, MessageListener
 		}
 	}
 
-	// TODO: act on failure by switching from secondary to primary if primary is down
 	@Scheduled(fixedDelay=200)
 	private void checkHeartbeat()
 	{
-		if(hasStarted && sender != null && !sender.isEmpty() && sender.equals(isPrimary ? "PRIMARY" : "SECONDARY") && lastTimeStamp !=null)
+		if(hasStarted && heartbeatSender != null && !heartbeatSender.isEmpty() && heartbeatSender.equals(isPrimary ? "PRIMARY" : "SECONDARY") && lastTimeStamp !=null)
 		{
 			long gap = Math.abs(ChronoUnit.MILLIS.between(lastTimeStamp, LocalDateTime.now()));
 			if(gap > heartbeatCheckMaximumInterval)
-				logger.error("Heartbeat error!! Sender: {}, lastTimeStamp: {}, gap: {}, maxInterval: {}", sender, lastTimeStamp, gap, heartbeatCheckMaximumInterval);
+				logger.error("Heartbeat error!! Sender: {}, lastTimeStamp: {}, gap: {}, maxInterval: {}", heartbeatSender, lastTimeStamp, gap, heartbeatCheckMaximumInterval);
 		}
 	}
 
@@ -81,6 +94,7 @@ public class HeartBeatServiceImpl implements HeartBeatService, MessageListener
 	public void start(boolean isPrimary)
 	{
 		this.isPrimary = isPrimary;
+		logger.info("Heartbeat service started running in {} mode with UID: {}", (isPrimary ? "PRIMARY" : "SECONDARY"), uid);
 		hasStarted = true;
 		ping();
 	}
